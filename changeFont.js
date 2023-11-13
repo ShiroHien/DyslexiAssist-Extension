7692c1c49e7f30d9a3d1574e9607af9310bb35ec
Shiro Hien <thaohien0607@gmail.com>
2023-11-13 22:54:34 +0700

    Update setting popup

--- changeFont.js
+++ changeFont.js
@@ -17,14 +17,26 @@ function changeFontToOpenDyslexic() {
 // Run the function to change the font
 changeFontToOpenDyslexic();
 
-chrome.storage.sync.get('font', function(data) {
-    if (data.font) {
-        // Apply the font setting
-        changeFontTo(data.font);
+// Listen for messages from the settings script
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    if (message.action === "changeFont") {
+        const newFont = message.font;
+        applyFontChange(newFont);
+    }
+    if (message.action === "changeColors") {
+        // Code to change colors
+        document.body.style.backgroundColor = message.bgColor;
+        document.body.style.color = message.textColor;
+        // You may need to apply this to other elements as well
     }
 });
 
-function changeFontTo(fontName) {
-    // Your code to change the font
+function applyFontChange(fontName) {
+    const styleElement = document.createElement('style');
+    document.head.appendChild(styleElement);
+    styleElement.sheet.insertRule(`
+        body, p, h1, h2, h3, h4, h5, h6, span, a, div {
+            font-family: 'Arial' !important;
+        }
+    `, 0);
 }
-
